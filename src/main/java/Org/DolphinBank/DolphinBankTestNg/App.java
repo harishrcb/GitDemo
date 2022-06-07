0dbf03a2a292d3f2d450a5cec82c0381c9faba3b
harish <kommalapatiharish.k@gmail.com>
2022-06-07 21:36:08 +0530

    first commit

---
+++ src/main/java/Org/DolphinBank/DolphinBankTestNg/App.java
@@ -0,0 +1,13 @@
+package Org.DolphinBank.DolphinBankTestNg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
